c4493261a3047b08b319991e8465051a5ca031c8
qixuan <1124035186@qq.com>
2021-07-05 16:54:09 +0800

    master_commit2

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -8,6 +8,7 @@ public class App
 {
     public static void main( String[] args )
     {
+        System.out.println("Third");
         System.out.println( "Hello World!" );
         System.out.println("厉害");
         System.out.println("1111");
