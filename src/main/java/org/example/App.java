f7b0e43035b46e91079774ecd5eac2889015e5eb
qixuan <1124035186@qq.com>
2021-07-05 15:43:48 +0800

    second_commit

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -9,5 +9,7 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+        System.out.println("厉害");
+        System.out.println("1111");
     }
 }
