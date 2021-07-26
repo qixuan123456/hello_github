14188eec674b0195726f630cac55c2547c5e514a
qixuan <1124035186@qq.com>
2021-07-26 21:34:30 +0800

    three

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,5 +1,7 @@
 package org.example;
 
+import java.util.Scanner;
+
 /**
  * Hello world!
  *
@@ -8,9 +10,23 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println("Third");
-        System.out.println( "Hello World!" );
-        System.out.println("厉害");
-        System.out.println("1111");
+        System.out.println("请输入账号:");
+        Scanner sc = new Scanner(System.in);
+        String name = sc.nextLine();
+        System.out.println("请输入密码:");
+        String password = sc.nextLine();
+        if("123".equals(name)){
+            if("123".equals(password)){
+                System.out.println("登陆成功!");
+                sc.nextLine();
+            }else {
+                System.out.println("账号或密码错误，请重试!");
+                main(null);
+            }
+        }else {
+            System.out.println("账号或密码错误，请重试!");
+            main(null);
+        }
+
     }
 }
